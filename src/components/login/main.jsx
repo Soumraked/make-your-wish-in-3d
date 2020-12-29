80763a6e9cb6d2a2fe7ad9067a684d490544d405
Danilo Ibáñez <danilobepo@gmail.com>
2020-12-29 03:03:03 -0300

    Validate email and wsp

--- src/components/login/main.jsx
+++ src/components/login/main.jsx
@@ -17,7 +17,6 @@ import axios from "axios";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 
-
 const useStyles = makeStyles((theme) => ({
   root: {
     "& .MuiTextField-root": {
@@ -73,68 +72,78 @@ export default function Main({ handleAccess, handleToken }) {
   var controller = new AbortController();
 
   const handleLogin = () => {
-    axios
-      .post(
-        `https://us-central1-u-app-3100e.cloudfunctions.net/api/admin/login`,
-        {
-          rut: nick,
-          password: values.password
-        }
-      )
-      .then((data) => {
-        setMessageName("");
-        setMessagePass("");
-        if (data.status === 200) {
-          handleToken(data.data.token);
-          handleAccess(1);
-          return controller.abort();
-        } else {
-          setMessageName(
-            "Error desconocido, verifique los datos antes de continuar."
-          );
-          setMessagePass(
-            "Error desconocido, verifique los datos antes de continuar."
-          );
-        }
-        setCharge(false);
-      })
-      .catch((error) => {
-        setMessageName("");
-        setMessagePass("");
-        console.log(error.request.status);
-        switch (error.request.status) {
-          case 400:
-            if (nick.length === 0) {
-              setMessageName("El nombre de usuario no puede estar vacio.");
-            }
-            if (values.password.length === 0) {
-              setMessagePass("La contraseña no puede estar vacia.");
-            }
-            if (nick.length !== 0 && values.password.length !== 0) {
-              setMessageName(" ");
-              setMessagePass(
-                "Las credenciales no coinciden, intenta nuevamente."
-              );
-            }
-            break;
-          case 404:
-            setMessageName("El rut no figura en la base de datos.");
-            break;
-          case 409:
-            setMessagePass("La contraseña ingresada es incorrecta.");
-            break;
-          default:
+    if (!validateEmail(nick)) {
+      setMessageName("Formato no valido para el email.");
+      setCharge(false);
+    } else {
+      axios
+        .post(
+          `https://us-central1-u-app-3100e.cloudfunctions.net/api/admin/login`,
+          {
+            rut: nick,
+            password: values.password
+          }
+        )
+        .then((data) => {
+          setMessageName("");
+          setMessagePass("");
+          if (data.status === 200) {
+            handleToken(data.data.token);
+            handleAccess(1);
+            return controller.abort();
+          } else {
             setMessageName(
               "Error desconocido, verifique los datos antes de continuar."
             );
             setMessagePass(
               "Error desconocido, verifique los datos antes de continuar."
             );
-            break;
-        }
-        setCharge(false);
-      });
+          }
+          setCharge(false);
+        })
+        .catch((error) => {
+          setMessageName("");
+          setMessagePass("");
+          console.log(error.request.status);
+          switch (error.request.status) {
+            case 400:
+              if (nick.length === 0) {
+                setMessageName("Email no puede estar vacio.");
+              }
+              if (values.password.length === 0) {
+                setMessagePass("La contraseña no puede estar vacia.");
+              }
+              if (nick.length !== 0 && values.password.length !== 0) {
+                setMessageName(" ");
+                setMessagePass(
+                  "Las credenciales no coinciden, intenta nuevamente."
+                );
+              }
+              break;
+            case 404:
+              setMessageName("El email no figura en la base de datos.");
+              break;
+            case 409:
+              setMessagePass("La contraseña ingresada es incorrecta.");
+              break;
+            default:
+              setMessageName(
+                "Error desconocido, verifique los datos antes de continuar."
+              );
+              setMessagePass(
+                "Error desconocido, verifique los datos antes de continuar."
+              );
+              break;
+          }
+          setCharge(false);
+        });
+    }
   };
+
+  const validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
   return (
     <>
 
